c0fd6fd85a193eefe4930d8b579c62144ee1b7ec
zakaria <erfan.yekezare@gmail.com>
2022-02-01 13:49:43 +0330

    takhati gar) Reversed list is now joined to a string.

--- python-Nobitex/takhati gar.py
+++ python-Nobitex/takhati gar.py
@@ -1,1 +1,1 @@
-print( sorted(list(x if ((ord(x)-97)%2 == 0) else x.upper() for x in (str(input()))), reverse=True) )
+print( " ".join( sorted( list(x if ((ord(x)-97)%2 == 0) else x.upper() for x in (str(input()))), reverse=True)) )
